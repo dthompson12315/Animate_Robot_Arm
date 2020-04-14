314cab65bb2c59b7a63e8e4e8ba6ac65c43548e7
dthompson12315 <dthompson2016@my.fit.edu>
2020-04-14 15:41:07 -0400

    test

--- Forward_Kinematics.py
+++ Forward_Kinematics.py
@@ -19,16 +19,36 @@ def main():
     end = vector(side, 1, side)
     zero = vector(0,20, 0)
 
-    scene2 = canvas(title='Collision Avoidance', width=1800, height=900, center=zero,
-                    background=color.gray(.2), forward=vector(0.5,-2,0), fov=70)
+    scene2 = canvas(title='Collision Avoidance',
+                    width=1800,
+                    height=900,
+                    center=zero,
+                    background=color.gray(.2),
+                    forward=vector(0.5,-2,0),
+                    fov=70)
 
-    floor = box(pos=vector(0, 0, 0), size=vector(s3, thk, s3), color=color.gray(1))
-    base = cylinder(axis=vector(0,1,0), pos=vector(0, thk, 0), color=color.red, radius=3,
+    floor = box(pos=vector(0, 0, 0),
+                size=vector(s3, thk, s3),
+                color=color.gray(1))
+    base = cylinder(axis=vector(0,1,0),
+                    pos=vector(0, thk, 0),
+                    color=color.red,
+                    radius=3,
                     length=5)
-    joint1 = sphere(pos=vector(0,base.length,0), radius=2.9, color=color.white)
-    arm1 = cylinder(axis=vector(0,1,0), pos=vector(0,joint1.pos.y + joint1.radius - .4,0), color=color.red, length=10)
-    joint2 = sphere(pos=vector(0,arm1.pos.y + arm1.length,0), radius=1.5, color=color.white)
-    arm2 = cylinder(axis=vector(0,1,0), pos=vector(0,joint2.pos.y,0), color=color.red, length=5)
+    joint1 = sphere(pos=vector(0,base.length,0),
+                    radius=2.9,
+                    color=color.white)
+    arm1 = cylinder(axis=vector(0,1,0),
+                    pos=vector(0,joint1.pos.y + joint1.radius - .4,0),
+                    color=color.red,
+                    length=10)
+    joint2 = sphere(pos=vector(0,arm1.pos.y + arm1.length,0),
+                    radius=1.5,
+                    color=color.white)
+    arm2 = cylinder(axis=vector(0,1,0),
+                    pos=vector(0,joint2.pos.y,0),
+                    color=color.red,
+                    length=5)
     # arm2.rotate(angle=vp.radians(1), axis=vector(1,0,0))
     dt = 0.001
 
